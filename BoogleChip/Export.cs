2760d275446e544d15079785993a74d23424570f
RatCarpet <rat@ratcarpet.com>
2022-04-07 23:09:51 -0400

    Bep

---
+++ BoogleChip/Export.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using System.Data;
+using System.Drawing;
+using System.Text.RegularExpressions;
+
+namespace BoogleChip
+{
+    internal class Export
+    {
+        public static string MakePng(KeyValuePair<string, DateTime> file, string folderPath)
+        {
+
+            string fileName = file.Key.ToString();
+            string fileDate = file.Value.ToString("yyyy-M-dd hhmmss");
+
+            string filePath = folderPath + "\\" + fileName;
+
+            FileInfo fileInfo = new FileInfo(filePath);
+
+            string imageDirectory = folderPath + "\\ExportedArt";
+            if (!Directory.Exists(imageDirectory))
+            {
+                Directory.CreateDirectory(imageDirectory);
+            }
+
+            string imageFileName = imageDirectory + "\\" + fileName.Substring(0, fileName.LastIndexOf('.')) + " " + fileDate + ".png";
+
+            //sqlite header
+            byte[] sqlHeader = new byte[] { 0x53, 0x51, 0x4c, 0x69, 0x74, 0x65, 0x20, 0x66, 0x6f, 0x72, 0x6d, 0x61, 0x74, 0x20, 0x33 };
+            int headerIndex = 0;
+            int headerLocation = 0;
+
+            FileStream fileStream = File.Open(filePath, FileMode.Open);
+
+            //Find sqlite header in clip file
+            while (true)
+            {
+                int fileByte = fileStream.ReadByte();
+
+                if (fileByte == -1)
+                {
+                    break;
+                }
+
+                if (fileByte == sqlHeader[headerIndex])
+                {
+                    headerIndex = headerIndex + 1;
+
+                    if (headerIndex == sqlHeader.Length)
+                    {
+                        //header found
+                        fileStream.Position = headerLocation;
+
+                        string sqliteFileName = filePath.Substring(0, filePath.LastIndexOf('.'));
+                        sqliteFileName = sqliteFileName + ".sqlite";
+
+                        FileStream temp = File.OpenWrite(sqliteFileName);
+                        fileStream.CopyTo(temp);
+                        temp.Close();
+                        fileStream.Close();
+
+                        //open sqlite connection
+                        SqliteConnectionStringBuilder conString = new SqliteConnectionStringBuilder();
+                        conString.DataSource = sqliteFileName;
+                        conString.DefaultTimeout = 5000;
+
+                        SqliteConnection dbcon = new SqliteConnection(conString.ToString());
+
+                        try
+                        {
+                            dbcon.Open();
+
+                            string query = "SELECT imageData From CanvasPreview";
+                            using (SqliteCommand cmd = dbcon.CreateCommand())
+                            {
+                                cmd.CommandText = query;
+                                FileStream output = File.OpenWrite(imageFileName);
+                                output.Write((System.Byte[])cmd.ExecuteScalar());
+                                output.Close();
+                                cmd.Dispose();
+                            }
+
+                        }
+                        catch (SqliteException ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+
+                        dbcon.Close();
+                        SqliteConnection.ClearAllPools();
+
+                        File.Delete(sqliteFileName);
+                        break;
+                    }
+                }
+                else
+                {
+                    headerLocation = headerLocation + headerIndex + 1;
+                    headerIndex = 0;
+                }                
+            }
+            return imageFileName;
+        }
+    }
+}
---
+++ BoogleChip/Program.cs
@@ -0,0 +1,105 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Data;
+using Tweetinvi;
+using Tweetinvi.Models;
+using Tweetinvi.Parameters;
+using Tweetinvi.Exceptions;
+
+namespace BoogleChip
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {     
+            //Boogle
+            string userId = Environment.UserName;
+            string folderPath = @"C:\Users\" + userId + @"\Desktop\ArtInProgress";
+            string logFilePath = folderPath + "\\BoogleChipLogFile.json";
+            string keyFilePath = folderPath + "\\BoogleChipKeyFile.json";           
+            
+            Dictionary<string, DateTime> currentFiles = new Dictionary<string, DateTime>();
+            Dictionary<string, DateTime> unchangedFiles = new Dictionary<string, DateTime>();
+            Dictionary<string, DateTime> changedFiles = new Dictionary<string, DateTime>();
+
+            //grab all .clib files from art directory
+            foreach (string f in Directory.GetFiles(folderPath))
+            {
+                if (Path.GetExtension(f) == ".clip")
+                {
+                    FileInfo fileInfo = new FileInfo(f);
+                    currentFiles.Add(fileInfo.Name, fileInfo.LastWriteTime);
+                }
+            }
+
+            //If log file does not exist create it and populate with current
+            if (!File.Exists(logFilePath))
+            {
+                File.Create(logFilePath).Close();
+                string jsonInitial = JsonConvert.SerializeObject(currentFiles, Formatting.Indented);
+                File.WriteAllText(logFilePath, jsonInitial);
+                changedFiles = currentFiles;
+            }
+            else
+            {
+                //Read old log and write to the "existing" dictionary
+                string getOldLog = File.ReadAllText(logFilePath);
+                unchangedFiles = JsonConvert.DeserializeObject<Dictionary<string, DateTime>>(getOldLog);
+
+                //compare existing log to current log 
+                changedFiles = currentFiles.Except(unchangedFiles).ToDictionary(x => x.Key, y => y.Value);
+                Dictionary<string, DateTime> newLog = changedFiles.Concat(unchangedFiles.Where(x => !changedFiles.Keys.Contains(x.Key))).ToDictionary(x => x.Key, y => y.Value);
+                //Write new log
+                string setJson = JsonConvert.SerializeObject(newLog);
+                File.WriteAllText(logFilePath, setJson);
+            }
+              
+            //Export png
+            List<string> imageFileNames = new List<string>();
+            foreach (KeyValuePair<string, DateTime> file in changedFiles)
+            {
+                imageFileNames.Add(Export.MakePng(file, folderPath));
+            }
+
+            //Grab keys from keyFile
+            TwitterKeys twitterKeys = new TwitterKeys();
+            if (!File.Exists(keyFilePath))
+            {
+                throw new FileNotFoundException(keyFilePath);
+            }
+            else
+            {
+                string getKeys = File.ReadAllText(keyFilePath);
+                twitterKeys = JsonConvert.DeserializeObject<TwitterKeys>(getKeys);                
+            }            
+            
+            //post to twitter
+            foreach (string fileName in imageFileNames)
+            {
+                FileInfo fileInfo = new FileInfo(fileName);
+                string tweetText = "WIP " + fileInfo.Name;
+                byte[] image = File.ReadAllBytes(fileName);
+
+                try
+                {
+                    TwitterClient userClient = new TwitterClient(twitterKeys.oauth_consumer_key, twitterKeys.oauth_consumer_secret, twitterKeys.oauth_token, twitterKeys.oauth_secret);
+                    IMedia uploadImage = await userClient.Upload.UploadTweetImageAsync(image);
+                    ITweet TweetImage = await userClient.Tweets.PublishTweetAsync(new PublishTweetParameters(tweetText) { Medias = { uploadImage } });
+                }
+                catch (TwitterException e)
+                {
+                    Console.WriteLine(e.ToString());
+                }                
+            }
+            Environment.Exit(0);
+        }
+
+        public class TwitterKeys
+        {
+            public string oauth_consumer_key { get; set; }
+            public string oauth_consumer_secret { get; set; }
+            public string oauth_token { get; set; }
+            public string oauth_secret { get; set; }
+        }
+    }
+}
